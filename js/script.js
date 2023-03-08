8cd22c1cbf9f84c12794fdd25a77106ab6820d23
EverettBernard <124380095+EverettBernard@users.noreply.github.com>
2023-03-08 16:06:00 +0000

    finnished i think

---
+++ js/script.js
@@ -0,0 +1,15 @@
+
+// Created by: Everett
+// Created on: Mar 2022
+// This file contains the JS functions for index.html
+
+function doMathClicked () {
+  // this function does basic math
+  document.getElementById('area').innerHTML = 'The area is ' + (4 * 9)
+}
+
+function doMathClicked1 () {
+  // this function does basic math
+  
+  document.getElementById('per').innerHTML = 'The perimiter is ' + (9*2+2*4)
+}
